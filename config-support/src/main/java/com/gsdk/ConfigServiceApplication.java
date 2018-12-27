b7094b9e0fd2b6fe52ceefe989ea9a70b1da8421
Carl <carol8421@126.com>
2018-12-27 18:05:05 +0800

    added configuration stuff.
    Signed-off-by: Carl <carol8421@126.com>

---
+++ config-support/src/main/java/com/gsdk/ConfigServiceApplication.java
@@ -0,0 +1,36 @@
+package com.gsdk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+@EnableConfigServer
+
+public class ConfigServiceApplication {
+
+//	@Bean
+//	@LoadBalanced
+//	RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
